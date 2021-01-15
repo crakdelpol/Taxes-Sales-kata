175928d96995fc0de88015b01d5e775938ec1506
Matteo Pipitone <matteo.pipitone@gmail.com>
2021-01-15 17:30:53 +0100

    first acceptance test (not compile)

---
+++ src/test/java/it/pipitone/matteo/taxes_sales_kata/AcceptanceTest.java
@@ -0,0 +1,28 @@
+package it.pipitone.matteo.taxes_sales_kata;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class AcceptanceTest {
+
+
+    @Test
+    public void firstAcceptanceTest(){
+
+        Bucket bucket = new Bucket("1 book at 12.49\n" +
+                "1 music CD at 14.99\n" +
+                "1 chocolate bar at 0.85");
+
+        String receipt = bucket.printReceipt();
+
+        assertThat(receipt, is("1 book: 12.49\n" +
+                "1 music CD: 16.49\n" +
+                "1 chocolate bar: 0.85\n" +
+                "Sales Taxes: 1.50\n" +
+                "Total: 29.83"));
+
+    }
+
+}
