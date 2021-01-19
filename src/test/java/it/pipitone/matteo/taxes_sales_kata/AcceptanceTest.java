cdc6685df000866b5f4f3e1e00e6cac9a76fac3d
Matteo Pipitone <matteo.pipitone@gmail.com>
2021-01-19 17:26:58 +0100

    hardcoded taxes

--- src/test/java/it/pipitone/matteo/taxes_sales_kata/AcceptanceTest.java
+++ src/test/java/it/pipitone/matteo/taxes_sales_kata/AcceptanceTest.java
@@ -3,15 +3,15 @@ package it.pipitone.matteo.taxes_sales_kata;
 import org.junit.Test;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class AcceptanceTest {
-
 
     @Test
     public void firstAcceptanceTest(){
@@ -38,14 +38,34 @@ public class AcceptanceTest {
                 "Total: 12.49"));
     }
 
+    @Test
+    public void parseOneItemWithTaxes() {
+        Bucket bucket = new Bucket("1 music CD at 14.99");
+        String receipt = bucket.printReceipt();
+        assertThat(receipt, is("1 music CD: 16.49\n"+
+                "Total: 16.49"));
+    }
+
+
     private static class Bucket {
 
         private final Item item;
 
         public Bucket(String items) {
-            String[] s = items.split(" ");
-            List<String> parameterPristine = Arrays.stream(s).filter(s1 -> !s1.equals("at")).collect(Collectors.toList());
-            item = new Item(Integer.parseInt(parameterPristine.get(0)), parameterPristine.get(1), new BigDecimal(parameterPristine.get(2)));
+            String regex = "(^[0-9]*) ([a-zA-Z ]*) (at) ([0-9.]*)";
+            Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+            Matcher matcher = pattern.matcher(items);
+
+            boolean found = matcher.find();
+            int itemNumber = Integer.parseInt(matcher.group(1));
+            String itemDescription = matcher.group(2);
+            BigDecimal price = new BigDecimal(matcher.group(4));
+            if(itemDescription.contains("book")){
+                item = new Item(itemNumber, itemDescription, price);
+            }else {
+                item = new Item(itemNumber, itemDescription, price.multiply(new BigDecimal("1.10"), new MathContext(4, RoundingMode.HALF_UP)));
+            }
+
         }
 
         public String printReceipt() {
